c390bff282e0553f3fb5b252cdf3d5498083e466
MariaJoseSolis <majosm96@gmail.com>
2017-12-01 17:49:47 -0600

    Re write JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,17 @@
-var fruits = [
-  "img/raspberry.png",
-  "img/orange.png",
-  "img/banana.png",
-  "img/apple.png",
-  "img/blue_berry.png",
-  "img/cherry.png",
-  "img/cherry.png",
-  "img/banana.png",
-  "img/grapes.png",
-  "img/lemon.png",
-  "img/lemon.png",
-  "img/raspberry.png",
-  "img/watermelon.png",
-  "img/strawberry.png",
-  "img/watermelon.png",
-  "img/orange.png",
-  "img/apple.png",
-  "img/blue_berry.png",
-  "img/strawberry.png",
-  "img/grapes.png"
-];
+// let countScore;
+// let time;
+let scoreIncrementer;
+let flippedCards;
+const score = document.getElementsById('score');
 
-var imgs = document.getElementsByClassName("fruits");
-console.log(imgs);
 
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
 
   // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
+  while (currentIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -44,8 +25,52 @@ function shuffle(array) {
   return array;
 }
 
-var newFruits = shuffle(fruits);
+function flipBack() {
+  flippedCards[0].classList.toggle('flipper-test');
+  flippedCards[1].classList.toggle('flipper-test');
 
-Array.from(imgs).forEach(function(element, index) {
-  element.src = newFruits[index];
-});
+  flippedCards = [];
+}
+
+function checkMatch() {
+  if (flippedCards[0].src === flippedCards[1].src) {
+    flippedCards = [];
+    const sumScore = scoreIncrementer + scoreIncrementer;
+    score.innerText = `0, ${sumScore}!`;
+  } else {
+    setTimeout(flipBack, 1500);
+  }
+}
+
+function flip() {
+  if (!this.classList.contains('flipper-test') && flippedCards.length < 2) {
+    this.classList.toggle('flipper-test');
+
+    flippedCards.push(this);
+
+    if (flippedCards.length === 2) {
+      checkMatch();
+    }
+  }
+}
+
+function createGameBoard() {
+  const imgs = document.getElementsByClassName('fruits');
+  const fruits = ['img/raspberry.png', 'img/orange.png', 'img/banana.png', 'img/apple.png', 'img/blue_berry.png', 'img/cherry.png', 'img/cherry.png', 'img/banana.png', 'img/grapes.png', 'img/lemon.png', 'img/lemon.png', 'img/raspberry.png', 'img/watermelon.png', 'img/strawberry.png', 'img/watermelon.png', 'img/orange.png', 'img/apple.png', 'img/blue_berry.png', 'img/strawberry.png', 'img/grapes.png'];
+
+  shuffle(fruits);
+
+  Array.from(imgs).forEach((element, index) => {
+    element.src = fruits[index];
+
+    if (imgs[index].classList.contains('flipper-test')) {
+      imgs[index].classList.toggle('flipped');
+    }
+    element.src = fruits[index];
+    element.addEventListener('click', flip);
+  });
+
+  score.innerText = '00';
+}
+
+createGameBoard();
